8a1c5e3f99b4fffdf1f6239d916a6354a7630b83
MahendraPachipala <mahendrapachipala123@gmail.com>
2025-11-16 20:12:20 +0530

    gemini 1.5 flash to 2.5 flash

--- utils/GeminiEmails.js
+++ utils/GeminiEmails.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_KEY);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
   async function gemini(prompt) {
     const chat = model.startChat({
